f25c7104517d524f76ab734dde7fd445b61edcf8
redcliff-op <devyashjangid@gmail.com>
2024-06-01 14:35:48 +0530

    SysNavBar: Change System's Navigation bar color to match background

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
 import NewsProvider from '@/providers/NewsProvider';
 import { Stack } from 'expo-router';
 import { View } from 'react-native';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function RootLayoutNav() {
+  NavigationBar.setBackgroundColorAsync('#161622');
   return (
     <View className='bg-background flex-1'>
       <NewsProvider>
